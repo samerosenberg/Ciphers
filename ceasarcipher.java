2e3eede882a6c886c559db1774db2faaf997cb85
Sam Rosenberg <serosenberg99@gmail.com>
2018-10-02 15:19:54 -0400

    Adding stuff in to make life easier

--- ceasarcipher.java
+++ ceasarcipher.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class ceasarcipher{
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    System.out.print("Would you like to 'encrypt' or 'decrypt': ");
+    System.out.println("Would you like to 'encrypt' or 'decrypt': ");
     String mode = scan.next();
+    
+    System.out.println("Enter in your message: ");
+    String message = scan.nextLine();
+    
+    System.out.println("What key value would you like to use: (0-25)");
+    int key = scan.nextInt();
   }
+  //create encrypt and decrypt methods 
 }
