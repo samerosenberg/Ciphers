83103ea917dee157d122f25ed262376e9d0003df
Sam Rosenberg <serosenberg99@gmail.com>
2018-10-02 15:16:02 -0400

    Just testing things out

---
+++ ceasarcipher.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ceasarcipher{
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Would you like to 'encrypt' or 'decrypt': ");
+    String mode = scan.next();
+  }
+}
